bf3cfb073925b86bf761ae7551cc51f30de9fae7
yuanwei <35893864+yuanwei1994@users.noreply.github.com>
2018-03-06 10:25:03 +0800

    CocoaPod 版本 - v 1.0.3 内存优化

--- YWLogisticsQuery/YWImageCache.m
+++ YWLogisticsQuery/YWImageCache.m
@@ -7,7 +7,6 @@
 //
 
 #import "YWImageCache.h"
-#import "YWConfigFile.h"
 
 @implementation YWImageCache
 
@@ -19,9 +18,10 @@
 #pragma mark - 异步加载
 - (void)startCacheImage:(NSString *)imageUrl
 {
-    WeakSelf;
-    self.imageUrl = imageUrl;
+    __typeof__(self) __weak wself = self;
     
+    self.imageUrl = imageUrl;
+
     // 先判断本地沙盒是否已经存在图像，存在直接获取，不存在再下载，下载后保存
     // 存在沙盒的Caches的子文件夹CacheImages中
     UIImage * image = [self loadLocalImage:imageUrl];
@@ -35,7 +35,7 @@
             NSData * imageData = [NSData dataWithContentsOfURL:[NSURL URLWithString:imageUrl]];
             
             // 缓存图片
-            [imageData writeToFile:[weakSelf imageFilePath:imageUrl] atomically:YES];
+            [imageData writeToFile:[wself imageFilePath:imageUrl] atomically:YES];
             
             // 回到主线程完成UI设置，也可以利用blcok,将image对象传到别处去
             dispatch_async(dispatch_get_main_queue(), ^{
